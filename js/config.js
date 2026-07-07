c069cc6ecf3a73efa98c20043affb312edbf5308
vxqj <tiktokalt1276@proton.me>
2026-07-07 12:16:03 +1000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,103 @@
+const CONFIG = {
 
+    site: {
+        name: "17Games",
+        game: "Bee ReMastered",
+        owner: "Humbe"
+    },
+
+
+    links: {
+        discord: "",
+        roblox: ""
+    },
+
+
+    staff: [
+
+        {
+            username: "itshumbe",
+            role: "Owner",
+            image: "CATBOX_LINK_HERE"
+        },
+
+        {
+            username: "q_qty",
+            role: "CEO",
+            image: "CATBOX_LINK_HERE"
+        },
+
+        {
+            username: "dark_gamer0yt",
+            role: "CTO",
+            image: "CATBOX_LINK_HERE"
+        },
+
+        {
+            username: "OneTech",
+            role: "COO",
+            image: "CATBOX_LINK_HERE"
+        },
+
+        {
+            username: "nikolajc2008",
+            role: "Head Of Staff",
+            image: "CATBOX_LINK_HERE"
+        },
+
+        {
+            username: "petere1711",
+            role: "Deputy Head Of Staff",
+            image: "CATBOX_LINK_HERE"
+        },
+
+        {
+            username: "30000000000000 / nathan",
+            role: "Assistant Head Of Staff | Creator Of Website",
+            image: "CATBOX_LINK_HERE"
+        }
+
+    ],
+
+
+    developers: [
+
+        {
+            username:"Humbe",
+            role:"Developer",
+            image:"CATBOX_LINK_HERE"
+        },
+
+        {
+            username:"dark_gamer0yt",
+            role:"Developer",
+            image:"CATBOX_LINK_HERE"
+        },
+
+        {
+            username:"print_invictus",
+            role:"Developer",
+            image:"CATBOX_LINK_HERE"
+        },
+
+        {
+            username:"q_qty",
+            role:"Developer",
+            image:"CATBOX_LINK_HERE"
+        },
+
+        {
+            username:"wdnter",
+            role:"Developer",
+            image:"CATBOX_LINK_HERE"
+        },
+
+        {
+            username:"stormy_vfx",
+            role:"Developer",
+            image:"CATBOX_LINK_HERE"
+        }
+
+    ]
+
+};
